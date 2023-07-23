72452736ea7406a35686ac118f0102c62811c6c8
marianne cuenco <marianne878@gmail.com>
2023-07-22 21:57:09 -0700

    my name

--- python_prework_assignment.py/prework_assignment.py
+++ python_prework_assignment.py/prework_assignment.py
@@ -1,4 +1,5 @@
 # Question 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function.
 
 def hello_name(user_name):
     print("hello_" + user_name.upper() + "!")
@@ -6,16 +7,19 @@ def hello_name(user_name):
 hello_name('marianne')
 
 # Question 2
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
 
 
 def first_odds():
     for value in range(1, 100):
         if value % 2 == 1:
             print(value)
+       
 
 first_odds()
 
 # Question 3
+# Please write a Python function, max_num_in_list to return the max number of a given list. 
 
 def max_num_in_list(a_list):
     max = a_list[0]
@@ -28,6 +32,7 @@ a_list = [1, 2, 3, 4, 5]
 print(max_num_in_list(a_list))
 
 # Question 4
+# Write a function to return if the given year is a leap year. The return should be booelean Type.
 
 def is_leap_year(a_year):
 
@@ -39,6 +44,8 @@ def is_leap_year(a_year):
 is_leap_year(2050)
 
 # 5 Question
+# Write a function to check to see if all numbers in list are consecutive numbers. For example, 
+# [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
 def is_consecutive(a_list):
     a_list = sorted(a_list)
